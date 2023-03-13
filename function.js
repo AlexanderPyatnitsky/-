6179e717f6fd58806f549a2bfc120b7f8524ffc9
AlexanderPyatnitsky <alexander.pyatnitsky@gmail.com>
2023-03-13 23:28:57 +0200

    function 2

--- function.js
+++ function.js
@@ -3,9 +3,30 @@
 * Напиши функцию calculateTotalPrice(items)
 * которая принимает масив цен(чисел) и возвращает их сумму.
 */
-const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
-let total = 0;
-for (const value of cart) {
-    total += value;
-}
-console.log(`Total: `, total);
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// let total = 0;
+// for (const value of cart) {
+//     total += value;
+// }
+// console.log(`Total: `, total);
+
+// ===================================================== //
+// const calculateTotalPrice = function (items) {
+//     console.log(`items в нутри функции: `, items);
+//     let total = 0;
+
+//     for (const item of items) {
+//         total += item;
+//     }
+
+//     return total;
+
+// };
+
+// const result1 = calculateTotalPrice([1, 2, 3]);
+// const result2 = calculateTotalPrice([5, 10, 15, 20]);
+// const result3 = calculateTotalPrice([100, 200, 300]);
+
+// console.log(`Общая сумма покупок: ${ result1 }`);
+// console.log(`Общая сумма покупок: ${ result2 }`);
+// console.log(`Общая сумма покупок: ${ result3 }`);
